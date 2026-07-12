2c1999a61b0f77d9e407f86a4c1b1eea416cac4b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "network_security_groups" {
-  description = "All network_security_group resources"
-  value       = azurerm_network_security_group.network_security_groups
-}
 output "network_security_groups_location" {
-  description = "List of location values across all network_security_groups"
-  value       = [for k, v in azurerm_network_security_group.network_security_groups : v.location]
+  description = "Map of location values across all network_security_groups, keyed the same as var.network_security_groups"
+  value       = { for k, v in azurerm_network_security_group.network_security_groups : k => v.location }
 }
 output "network_security_groups_name" {
-  description = "List of name values across all network_security_groups"
-  value       = [for k, v in azurerm_network_security_group.network_security_groups : v.name]
+  description = "Map of name values across all network_security_groups, keyed the same as var.network_security_groups"
+  value       = { for k, v in azurerm_network_security_group.network_security_groups : k => v.name }
 }
 output "network_security_groups_resource_group_name" {
-  description = "List of resource_group_name values across all network_security_groups"
-  value       = [for k, v in azurerm_network_security_group.network_security_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all network_security_groups, keyed the same as var.network_security_groups"
+  value       = { for k, v in azurerm_network_security_group.network_security_groups : k => v.resource_group_name }
 }
 output "network_security_groups_security_rule" {
-  description = "List of security_rule values across all network_security_groups"
-  value       = [for k, v in azurerm_network_security_group.network_security_groups : v.security_rule]
+  description = "Map of security_rule values across all network_security_groups, keyed the same as var.network_security_groups"
+  value       = { for k, v in azurerm_network_security_group.network_security_groups : k => v.security_rule }
 }
 output "network_security_groups_tags" {
-  description = "List of tags values across all network_security_groups"
-  value       = [for k, v in azurerm_network_security_group.network_security_groups : v.tags]
+  description = "Map of tags values across all network_security_groups, keyed the same as var.network_security_groups"
+  value       = { for k, v in azurerm_network_security_group.network_security_groups : k => v.tags }
 }
 
