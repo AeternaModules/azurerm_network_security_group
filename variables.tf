e26ee4eb0c781c0bd794dd12d20338e014b7fd25
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:36:25 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -8,17 +8,17 @@ Required:
 Optional:
     - tags
     - security_rule (block):
-        - access (required)
+        - access (optional)
         - description (optional)
         - destination_address_prefix (optional)
         - destination_address_prefixes (optional)
         - destination_application_security_group_ids (optional)
         - destination_port_range (optional)
         - destination_port_ranges (optional)
-        - direction (required)
-        - name (required)
-        - priority (required)
-        - protocol (required)
+        - direction (optional)
+        - name (optional)
+        - priority (optional)
+        - protocol (optional)
         - source_address_prefix (optional)
         - source_address_prefixes (optional)
         - source_application_security_group_ids (optional)
@@ -32,17 +32,17 @@ EOT
     resource_group_name = string
     tags                = optional(map(string))
     security_rule = optional(list(object({
-      access                                     = string
+      access                                     = optional(string)
       description                                = optional(string)
       destination_address_prefix                 = optional(string)
       destination_address_prefixes               = optional(set(string))
       destination_application_security_group_ids = optional(set(string))
       destination_port_range                     = optional(string)
       destination_port_ranges                    = optional(set(string))
-      direction                                  = string
-      name                                       = string
-      priority                                   = number
-      protocol                                   = string
+      direction                                  = optional(string)
+      name                                       = optional(string)
+      priority                                   = optional(number)
+      protocol                                   = optional(string)
       source_address_prefix                      = optional(string)
       source_address_prefixes                    = optional(set(string))
       source_application_security_group_ids      = optional(set(string))
